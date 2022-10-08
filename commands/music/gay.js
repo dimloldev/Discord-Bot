119a7fce58690d9c331950cc8b056798e56606f5
prionetodev <wot.mits@gmail.com>
2022-10-08 20:25:18 +0200

    Delete gay.js

--- commands/music/gay.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'gay',
-    aliases: [],
-    utilisation: '{prefix}gay',
-
-    execute(client, message) {
-        message.channel.send(`${message.author}, είσαι λιγο gay!🍆🔞`);
-    },
-};
